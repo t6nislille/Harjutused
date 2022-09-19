42a3ba326dbc42b7e3794742f2ecde1a21b46014
Tõnis <tonislille@gmail.com>
2022-09-19 14:25:25 +0300

    first commit

---
+++ 0101.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ülesanded</title>
+</head>
+<body>
+<?php
+
+echo "ÜLESANNE: <br>Järgnevas ülesandes tohib kasutada vaid IF tingimust ja FOR tsükklit. Arvude üleühe kuvamiseks kasutage lisamuutujat. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19<br>";
+
+echo "LAHENDUS:<br>";
+for ($i = 1; $i < 20; $i += 2) {
+    echo "$i<br>";
+}
+?>
+
+</body>
+</html>
---
+++ 0103.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>0102</title>
+</head>
+<body>
+    <?php
+    echo "LAHENDUS: <br>";
+    for ( $i = 100; $i <= 1000; $i += 100){
+        echo "$i<br>";
+    }
+    ?>
+</body>
+</html>
